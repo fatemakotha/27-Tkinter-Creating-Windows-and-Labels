c8e30bf9943229942e229468d52031f4ee405fcf
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-21 12:24:41 +0600

    Changed the title of the window

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import tkinter
 
 window = tkinter.Tk() #created a window
+window.title("My First GUI prog") #changes the title of the window
 
-window.mainloop() #will keep the window open. This is a loop lie: while True
+
+
+
+window.mainloop() #will keep the window open. This is a loop lie: while True. HAS TO BE AT THE END OF THE PROGRAM **
