6dd001ab75ae32892b98cadb21e860cfc162b8f7
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-21 12:28:59 +0600

    Created a label

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import tkinter
 
-window = tkinter.Tk() #created a window
-window.title("My First GUI prog") #changes the title of the window
-window.minsize(width=500, height=300)
+my_window = tkinter.Tk() #created a window
+my_window.title("My First GUI prog") #changes the title of the window
+my_window.minsize(width=500, height=300) #changes the size of the window
+
+#Creating a LABEL:
+
+my_label = tkinter.Label(text="I am a Label")
+my_label.pack() #pust the label at the center of screen
 
 
 
-window.mainloop() #will keep the window open. This is a loop lie: while True. HAS TO BE AT THE END OF THE PROGRAM **
+
+
+my_window.mainloop() #will keep the window open. This is a loop lie: while True. HAS TO BE AT THE END OF THE PROGRAM **
